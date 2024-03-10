208c0fd5af96ac1704f8b2e54e0d5b5afaf7a201
Bholi <najmulbro123@gmail.com>
2024-03-10 12:39:48 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+year = int(input('Enter the year : '))
+a = True
+b = True
+c = True
+if year % 4 == 0:
+    a = True
+else:
+    a = False
+if year % 100 == 0:
+    b = False
+else:
+    b = True
+if year % 400 == 0:
+    c = True
+else:
+    c = False
+
+if a and b:
+    print('This is leap year')
+elif a and c:
+    print('This leap year')
+elif b and c:
+    print('This is leap year')
+else:
+    print('This is not a leap year')
